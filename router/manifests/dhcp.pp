273a4f5aa17edd001180d5e140bfef50b4c1bf1f
xing <xing@router.(none)>
2011-11-02 15:39:29 +0000

    Dynamic DNS update

--- router/manifests/dhcp.pp
+++ router/manifests/dhcp.pp
@@ -19,10 +19,18 @@ class dhcp {
     require => Package['dhcp3-server']
   }
   
+  file {'/etc/dhcp/dns-dhcp.key':
+    ensure => file,
+    source => '/etc/bind/dns-dhcp.key',
+    require => Class['dns'],
+    owner => root,
+    group => root,
+  }
+  
   service {'isc-dhcp-server':
     ensure => running,
     enable => true,
-    require => Class["adapters"],
-    subscribe => [File['/etc/dhcp/dhcpd.conf'], File['/etc/default/isc-dhcp-server']]
+    require => [Class["adapters"], Class["dns"]],
+    subscribe => [File['/etc/dhcp/dhcpd.conf'], File['/etc/default/isc-dhcp-server'], File['/etc/dhcp/dns-dhcp.key']]
   }
 }
--- router/manifests/dns.pp
+++ router/manifests/dns.pp
@@ -1,6 +1,20 @@
 class dns {
+  $keygen_cmd = 'rndc-confgen -a -b 512 -k dhcpupdate -r /dev/urandom -c /etc/bind/dns-dhcp.key'
+
   package {'bind9':
     ensure => installed
+  }
+ 
+  exec {$keygen_cmd:    
+    path => '/usr/sbin',
+    creates => '/etc/bind/dns-dhcp.key',
+    require => Package['bind9']
+  }
+  
+  file {'/etc/bind/dns-dhcp.key':
+    ensure => present,
+    mode => 644,
+    require => Exec[$keygen_cmd],
   }
   
   file {'/etc/bind/named.conf.local':
@@ -42,6 +56,7 @@ class dns {
   service {'bind9':
     ensure => running,
     enable => true,
+    require => File['/etc/bind/dns-dhcp.key'],
     subscribe => [File['/etc/bind/named.conf.local'], File['/etc/bind/named.conf.options'], File['/etc/bind/db.example.local'], File['/etc/bind/db.192']]
   }
 }
